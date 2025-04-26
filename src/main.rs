0d3f79e98f81c632901639c76b29c0eadf3fc40d
IsakBH <isak@brunhenriksen.net>
2025-04-26 15:07:09 +0200

    Fjernet knapp, lagt til text input field + lagt til assets og icon mapper

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,31 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{glib, Application, ApplicationWindow, Entry};
 
 const APP_ID: &str = "no.brunhenriksen.ordKode";
 
 fn main() -> glib::ExitCode {
-    // lag ny applikasjon
     let app = Application::builder().application_id(APP_ID).build();
-
-    // connect til "activate" signalet av 'app'
     app.connect_activate(build_ui);
-
-    // kjør applikasjonen
     app.run()
 }
 
 fn build_ui(app: &Application) {
-    // lag en knapp med label og margins
-    let button = Button::builder()
-        .label("Trykk på meg!")
-        .margin_top(24)
-        .margin_bottom(24)
-        .margin_start(24)
-        .margin_end(24)
+    // lag text input field
+    let entry = Entry::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .placeholder_text("Skriv noe drit her...") // placeholder text
         .build();
 
-    button.connect_clicked(|button| {
-        button.set_label("Heisan sveisan, verden!");
-    });
-
-    // lag vindu og sett tittel på han
+    // lag vinduet
     let window = ApplicationWindow::builder()
         .application(app)
         .title("ordKode")
-        .child(&button)
+        .child(&entry)
         .build();
 
     // vis vinduet
-    window.present()
+    window.present();
 }
